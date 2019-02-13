0972055de8351808b7e1d4dfee43e57c57e491cf
André Yuri <andre_yuri@live.com>
2019-02-12 23:04:42 -0200

    mensagens de erro, serviço de ciclo de pagamento e resumo, correção bug conexão com mongodb, rotas

--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
+
 module.exports = 
     // MongoDB connection
     mongoose.connect(
-        "mongodb+srv://nodeapi:" + 
-        process.env.MONGO_ATLAS_PW + 
-        "@node-api-shop-39t16.mongodb.net/test?retryWrites=true",
+        `mongodb://meanstack:meanstack123@ds133865.mlab.com:33865/mean-stack`,
         { useNewUrlParser: true }
     );
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'."
+mongoose.Error.messages.String.enum = "'{VALUE}' não é válido para o atributo '{PATH}'."
